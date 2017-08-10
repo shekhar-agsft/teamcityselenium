f81be576f5a3678bedad2b26de56dbeabfd89b1d
shekhar-agsft <shekharc2229@gmail.com>
2017-08-10 17:24:19 +0530

    test commit

--- seleniumproject/EbayAdvancedSearch/src/test/java/ru/parsentev/app/EbayAdvancedSearch.java
+++ seleniumproject/EbayAdvancedSearch/src/test/java/ru/parsentev/app/EbayAdvancedSearch.java
@@ -9,40 +9,37 @@ import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 import static org.junit.Assert.*;
+
+import java.sql.Driver;
 import java.util.regex.Pattern;
 import static org.apache.commons.lang3.StringUtils.join;
 
 public class EbayAdvancedSearch {
 	private Selenium selenium;
 
+	WebDriver driver;
 	@Before
 	public void setUp() throws Exception {
-		WebDriver driver = new FirefoxDriver();
+		 driver = new FirefoxDriver();
 		String baseUrl = "http://www.google.com/";
-		selenium = new WebDriverBackedSelenium(driver, baseUrl);
+		//selenium = new WebDriverBackedSelenium(driver, baseUrl);
 	}
 
 	@Test
 	public void testEbayAdvancedSearch() throws Exception {
-		selenium.open("/");
-		selenium.waitForPageToLoad("30000");
-		selenium.click("id=gh-as-a");
-		selenium.type("id=_nkw", "JUnit");
-		selenium.select("id=e1-1", "value=267");
-		selenium.click("id=LH_TitleDesc");
-		selenium.click("css=button.btn.btn-prim");
-		selenium.waitForPageToLoad("30000");
-		for (int second = 0;; second++) {
-			if (second >= 60) fail("timeout");
-			try { if (selenium.isElementPresent("css=span.listingscnt")) break; } catch (Exception e) {}
-			Thread.sleep(1000);
-		}
+		
+		driver.navigate().to("http://www.google.com/");
+        System.out.println(driver.getTitle());
+        assertTrue( driver.getTitle().compareTo("Google")==0 );
 
-		assertEquals("Объявлений: 488", selenium.getText("css=span.listingscnt"));
 	}
 
 	@After
 	public void tearDown() throws Exception {
-		selenium.stop();
+		
+		if(driver!=null)
+		{
+			driver.close();
+		}
 	}
 }
